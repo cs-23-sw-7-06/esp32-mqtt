65728dceac84df73aae5989a323e93c5860c5c8e
mast3r <thomasmollerjensen@live.dk>
2023-10-30 15:57:46 +0100

    initial wifi setup

--- main/main.cpp
+++ main/main.cpp
@@ -4,68 +4,130 @@
 #include "sdkconfig.h"
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include <driver/gpio.h>
-#include <vector>
+#include <mqtt_client.h>
+#include <esp_wifi.h>
+#include <nvs.h>
+#include <nvs_flash.h>
 
-constexpr const char* letters[] = {
-".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", // A-I
-".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", // J-R
-"...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." // S-Z
-};
+#include <iostream>
 
-extern "C" void app_main(void)
-{
-  std::vector<char> message;
-  message.push_back('S');
-  message.push_back('O');
-  message.push_back('S');
-  message.push_back('\0');
+using namespace std;
 
-  gpio_reset_pin(blink_port);
-  gpio_set_direction(blink_port, GPIO_MODE_OUTPUT);
+#define DELAY 250 // arbitrary tick delay to make sure stuff is properly initialized before proceeding to the next boot step
 
-  while (true) {
-    flash_morse(message.data());
-  }
+void print_err(esp_err_t err){
+	cout << "Error!" << endl << "\t\033[38;2;255;0;0m";
+	switch (err){
+		case ESP_ERR_WIFI_NOT_INIT:
+			cout << "Wifi is not initialized!";
+			break;
+		case ESP_ERR_INVALID_ARG:
+			cout << "Invalid arguments!";
+			break;
+		case ESP_ERR_NO_MEM:
+			cout << "Out of memory!";
+			break;
+		case ESP_ERR_WIFI_CONN:
+			cout << "Internal WiFi error!";
+			break;
+		case ESP_FAIL:
+			cout << "Other error!";
+			break;
+		case ESP_ERR_NVS_NOT_INITIALIZED:
+			cout << "NVS not initialized!";
+			break;
+		case ESP_ERR_NVS_NO_FREE_PAGES:
+			cout << "NVS Storage has no empty pages!";
+			break;
+		case ESP_ERR_NOT_FOUND:
+			cout << "No NVS partition!";
+			break;
+		default:
+			cout << "Error not configured: " << err;
+			break;
+	}
+	cout << "\033[0m" << endl;
 }
 
-void flash_morse(const char* message) {
-  for (int i = 0; message[i] != 0; i++) {
-    auto morse_snippet = letter_to_morse(message[i]);
-    for (int j = 0; morse_snippet[j]; j++) {
-      flash_dot_or_dash(morse_snippet[j]);
-    }
-    flash_dot_or_dash(',');
-  }
-  flash_dot_or_dash(' ');
+void print_ok(){
+	cout << "\033[38;2;0;255;0m \tSuccess!\033[0m" << endl;
 }
 
-const char* letter_to_morse(char c) {
-  if (c >= 'a' && c <= 'z') {
-    return letters[c - 'a'];
-  }
-
-  return letters[c - 'A'];
+void mqtt_published_handler(){
+	cout << "published" << endl;
 }
 
-void flash_dot_or_dash(char c) {
-  switch (c) {
-    case ' ':
-      delay(space_delay);
-    break;
-    case ',':
-      delay(shortspace_delay);
-    break;
-    case '.':
-    case '-':
-      gpio_set_level(blink_port, 1);
-      delay(c == '.' ? dot_delay : dash_delay);
-      gpio_set_level(blink_port, 0);
-      delay(300);
-    break;
-  }
-}
+extern "C" void app_main(void) {
+	esp_err_t err;
 
-void delay(uint32_t milis) {
-  vTaskDelay(milis / portTICK_PERIOD_MS);
+// ################################ NVS INITIALIZATION ################################
+	cout << "Initializing NVS partition" << endl;
+	if(!(err = nvs_flash_init())){
+		print_ok();
+	} else print_err(err);
+	vTaskDelay(DELAY);
+// ################################ END NVS INITIALIZATION ################################
+
+// ################################ WIFI INITIALIZATION ################################
+	cout << "Configuring WiFi" << endl;
+	wifi_init_config_t wifi_init_config = WIFI_INIT_CONFIG_DEFAULT();
+
+	wifi_config_t wifi_config = {
+		.sta = {
+			.ssid = "p7-hotspot",
+			.scan_method = WIFI_FAST_SCAN
+		}
+	};
+	vTaskDelay(DELAY);
+
+	cout << "Initializing WiFi...";
+	if(!(err = esp_wifi_init(&wifi_init_config))){
+		print_ok();
+	} else print_err(err);
+	vTaskDelay(DELAY);
+
+	cout << "Setting WiFi configuration...";
+	if(!(err = esp_wifi_set_config(WIFI_IF_STA, &wifi_config))){
+		print_ok();
+	} else print_err(err);
+
+	cout << "Starting WiFi...";
+	if(!(err = esp_wifi_start())){
+		print_ok();
+	} else print_err(err);
+	vTaskDelay(DELAY);
+
+	cout << "Connecting WiFi...";
+	if(!(err = esp_wifi_connect())){
+		print_ok();
+	} else print_err(err);
+	vTaskDelay(DELAY);
+
+// ################################ END WIFI INITIALIZATION ################################
+
+// ################################ MQTT INITIALIZATION ################################
+	cout << "Configuring MQTT" << endl;
+	const esp_mqtt_client_config_t mqtt_config = {
+		.broker = {
+			.address = {
+				.uri = "mqtt://10.42.0.1:4000"
+			}
+		}
+	};
+	vTaskDelay(DELAY);
+
+	cout << "Initializing MQTT" << endl;
+	esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_config);
+
+	esp_mqtt_client_register_event(client, MQTT_EVENT_PUBLISHED, (esp_event_handler_t) mqtt_published_handler, client);
+	vTaskDelay(DELAY);
+
+	cout << "Starting MQTT";
+	if(!(err = esp_mqtt_client_start(client))){
+		cout << "Success!" << endl;
+	} else print_err(err);
+	vTaskDelay(DELAY);
+// ################################ END MQTT INITIALIZATION ################################
+
+
 }
---
+++ run.sh
@@ -0,0 +1,5 @@
+idf.py build
+
+idf.py -p $1 flash
+#screen $1 115200
+
