2236409dc2e3767694cb94c5a70df4f3292d26a9
Mast3r_waf1z <thomasmollerjensen@live.dk>
2023-11-06 01:27:30 +0100

    implemented wifi and mqtt connection establishment

--- main/main.cpp
+++ main/main.cpp
@@ -1,175 +1,184 @@
-#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
 
-#include <inttypes.h>
-#include <sdkconfig.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <freertos/event_groups.h>
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
 #include <mqtt_client.h>
-#include <esp_wifi.h>
-#include <esp_system.h>
-#include <esp_event.h>
-#include <nvs.h>
-#include <nvs_flash.h>
+#include <mqtt5_client.h>
+#include <mqtt_supported_features.h>
 
-#include <iostream>
+#define WIFI_SSID      CONFIG_ESP_WIFI_SSID
+#define WIFI_PASSWORD      CONFIG_ESP_WIFI_PASSWORD
+#define MAX_RETRIES  CONFIG_ESP_MAXIMUM_RETRY
 
-#define DELAY 250 // arbitrary tick delay to make sure stuff is properly initialized before proceeding to the next boot step
-#define WIFI_SUCCESS_BIT BIT0
-#define WIFI_FAIL_BIT BIT1
+#if CONFIG_ESP_WPA3_SAE_PWE_HUNT_AND_PECK
+#define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_HUNT_AND_PECK
+#define EXAMPLE_H2E_IDENTIFIER ""
+#elif CONFIG_ESP_WPA3_SAE_PWE_HASH_TO_ELEMENT
+#define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_HASH_TO_ELEMENT
+#define EXAMPLE_H2E_IDENTIFIER CONFIG_ESP_WIFI_PW_ID
+#elif CONFIG_ESP_WPA3_SAE_PWE_BOTH
+#define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_BOTH
+#define EXAMPLE_H2E_IDENTIFIER CONFIG_ESP_WIFI_PW_ID
+#endif
+#if CONFIG_ESP_WIFI_AUTH_OPEN
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_OPEN
+#elif CONFIG_ESP_WIFI_AUTH_WEP
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WEP
+#elif CONFIG_ESP_WIFI_AUTH_WPA_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA2_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA_WPA2_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA_WPA2_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA3_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA3_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA2_WPA3_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_WPA3_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WAPI_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WAPI_PSK
+#endif
 
-uint8_t ssid[32] = "p7-hotspot";
-uint8_t password[32] = "";
+static EventGroupHandle_t s_wifi_event_group;
 
-uint8_t max_retries = 10;
-uint8_t retries = 0;
+#define WIFI_CONNECTED      BIT0
+#define WIFI_FAILED         BIT1
 
-EventGroupHandle_t event_group;
+static const char *TAG = "Measurement Node";
+
+static int s_retry_num = 0;
+
+esp_mqtt_client_handle_t mqtt_client;
 
 
+void nvs_init(void){
+    ESP_LOGI(TAG, "Initializing NVS");
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
 
-void try_exec(esp_err_t err){
-	if(err == ESP_OK) return;
+}
 
-	std::cout << "Error!" << std::endl << "\t\033[38;2;255;0;0m";
-	switch (err){
-		case ESP_ERR_WIFI_NOT_INIT:
-			std::cout << "Wifi is not initialized!";
-			break;
-		case ESP_ERR_INVALID_ARG:
-			std::cout << "Invalid arguments!";
-			break;
-		case ESP_ERR_NO_MEM:
-			std::cout << "Out of memory!";
-			break;
-		case ESP_ERR_WIFI_CONN:
-			std::cout << "Internal WiFi error!";
-			break;
-		case ESP_FAIL:
-			std::cout << "Other error!";
-			break;
-		case ESP_ERR_NVS_NOT_INITIALIZED:
-			std::cout << "NVS not initialized!";
-			break;
-		case ESP_ERR_NVS_NO_FREE_PAGES:
-			std::cout << "NVS Storage has no empty pages!";
-			break;
-		case ESP_ERR_NOT_FOUND:
-			std::cout << "No NVS partition!";
-			break;
-		default:
-			std::cout << "Error not configured: " << err;
-			break;
-	}
-	std::cout << "\033[0m" << std::endl;
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+    ESP_LOGI(TAG, "WIFI event handler called");
+    if (event_base == WIFI_EVENT){
+        switch(event_id){
+            case WIFI_EVENT_STA_START:
+                esp_wifi_connect();
+                break;
+            case WIFI_EVENT_STA_DISCONNECTED:
+                if (s_retry_num < MAX_RETRIES) {
+                    esp_wifi_connect();
+                    s_retry_num++;
+                    ESP_LOGW(TAG, "retry to connect to the AP, retries: %d", s_retry_num);
+                } else {
+                    xEventGroupSetBits(s_wifi_event_group, WIFI_FAILED);
+                }
+                ESP_LOGE(TAG,"connect to the AP fail");
+                break;
+        }
+    } 
+    else if (event_base == IP_EVENT){
+        switch(event_id){
+            case IP_EVENT_STA_GOT_IP:
+                ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+                ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+                s_retry_num = 0;
+                xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED);
+                break;
+        }
+    }
+    else{
+        ESP_LOGW(TAG, "Unhandled WiFi event ID: %ld", event_id);
+    }
+}
+
+static void mqtt_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data){
+    ESP_LOGI(TAG, "MQTT event handler called");
+    switch(event_id){
+        case MQTT_EVENT_CONNECTED:
+            ESP_LOGI(TAG, "MQTT Connected!");
+            break;
+        default:
+            ESP_LOGW(TAG, "Unhandled MQTT Event ID: %ld", event_id);
+            break;
+    }
+}
+
+void wifi_init(void){
+    ESP_LOGI(TAG, "Initializing WiFi");
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_LOGI(TAG, "Initializing TCP/IP stack");
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_LOGI(TAG, "Initializing ESP event loop");
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    ESP_LOGI(TAG, "Defining Wifi configs");
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASSWORD,
+            .threshold = {
+                .authmode = ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD
+            },
+            .sae_pwe_h2e = ESP_WIFI_SAE_MODE,
+            .sae_h2e_identifier = EXAMPLE_H2E_IDENTIFIER,
+        },
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_LOGI(TAG, "Creating event handler instances");
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, &instance_got_ip));
+
+    ESP_LOGI(TAG, "Setting configurations");
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+
+    ESP_LOGI(TAG, "Finished initializing WiFi");
+}
+
+void wifi_start(void){
+    ESP_LOGI(TAG, "Starting WiFi");
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "Wifi initialization finished.");
+
+    ESP_LOGI(TAG, "Waiting for event bits");
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED | WIFI_FAILED, pdFALSE, pdFALSE, portMAX_DELAY);
+
+    if (bits & WIFI_CONNECTED) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s", WIFI_SSID, WIFI_PASSWORD);
+    } else if (bits & WIFI_FAILED) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s", WIFI_SSID, WIFI_PASSWORD);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+    ESP_LOGI(TAG, "Successfully connected to WiFi");
 }
 
 
-void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data){
-	std::cout << "event handler called" << std::endl;
-	switch(event_id){
-		case WIFI_EVENT_STA_START:
-			std::cout << "Connecteding!" << std::endl;
-			try_exec(esp_wifi_connect());
-			break;
-		case WIFI_EVENT_STA_DISCONNECTED:
-			std::cout << "Disconnected! retrying...";
-			printf("%d", retries);
-			std::cout << std::endl;
-			retries++;
-			if(retries <= max_retries){
-				try_exec(esp_wifi_connect());
-				vTaskDelay(DELAY);
-			}
-			else{
-				xEventGroupSetBits(event_group, WIFI_FAIL_BIT);
-			}
-			break;
-		case IP_EVENT_STA_GOT_IP:
-			retries = 0;
-			//ip_event_got_ip_t *event = (ip_event_got_ip_t*) event_data;
-			std::cout << "Got IP Address: " 
-				//<< IP2STR(&event->ip_info.ip) 
-				<< std::endl;
-			xEventGroupSetBits(event_group, WIFI_SUCCESS_BIT);
-			break;
-	}
-}
-
-extern "C" void app_main(void) {
-
-// ################################ NVS INITIALIZATION ################################
-	std::cout << "Initializing NVS partition" << std::endl;
-	esp_err_t ret = nvs_flash_init();
-	if(ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND){
-		try_exec(nvs_flash_erase());
-		try_exec(nvs_flash_init());
-	}
-
-	vTaskDelay(DELAY);
-// ################################ END NVS INITIALIZATION ################################
-
-// ################################ WIFI INITIALIZATION ################################
-	std::cout << "Configuring WiFi" << std::endl;
-	wifi_init_config_t wifi_init_config = WIFI_INIT_CONFIG_DEFAULT();
-
-	uint8_t MAC[6] = {0xb8, 0x8a, 0x60, 0x4d, 0x4b, 0x62};
-
-	wifi_config_t wifi_config = {
-		.sta = {
-			.ssid = *ssid,
-			.threshold = {
-				.authmode = WIFI_AUTH_OPEN
-			}
-		}
-	};
-
-	event_group = xEventGroupCreate();
-	vTaskDelay(DELAY);
-
-	std::cout << "Initializing WiFi...";
-	try_exec(esp_netif_init());
-
-	try_exec(esp_event_loop_create_default());
-
-	esp_netif_t *network = esp_netif_create_default_wifi_sta();
-
-	esp_event_handler_instance_t instance_any_id;
-	esp_event_handler_instance_t instance_got_ip;
-
-	try_exec(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_any_id));
-	try_exec(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, &instance_got_ip));
-
-	try_exec(esp_wifi_init(&wifi_init_config));
-
-	vTaskDelay(DELAY);
-
-	std::cout << "Setting WiFi configuration...";
-	try_exec(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
-
-	try_exec(esp_wifi_set_mode(WIFI_MODE_STA));
-
-	std::cout << "Starting WiFi...";
-	try_exec(esp_wifi_start());
-
-	EventBits_t bits = xEventGroupWaitBits(event_group, WIFI_SUCCESS_BIT | WIFI_FAIL_BIT, pdFALSE, pdFALSE, portMAX_DELAY);
-
-	if(bits & WIFI_SUCCESS_BIT){
-		std::cout << "Successfully connected to: " << ssid << std::endl;
-	}
-	else if(bits & WIFI_FAIL_BIT){
-		std::cout << "Failed to connect to: " << ssid << std::endl;
-	}
-	else {
-		std::cout << "Unknown error!" << std::endl;
-	}
-
-	vTaskDelay(DELAY);
-
-// ################################ END WIFI INITIALIZATION ################################
-
-// ################################ MQTT INITIALIZATION ################################
-	/*std::cout << "Configuring MQTT" << std::endl;
+void mqtt_init(void){	
+    ESP_LOGI(TAG, "Configuring MQTT client");
 	const esp_mqtt_client_config_t mqtt_config = {
 		.broker = {
 			.address = {
@@ -177,19 +186,30 @@ extern "C" void app_main(void) {
 			}
 		}
 	};
-	vTaskDelay(DELAY);
 
-	std::cout << "Initializing MQTT" << std::endl;
-	esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_config);
+	ESP_LOGI(TAG, "Initializing MQTT client");
+	mqtt_client = esp_mqtt_client_init(&mqtt_config);
 
-	//try_exec(esp_mqtt_client_register_event(client, MQTT_EVENT_PUBLISHED, (esp_event_handler_t) mqtt_published_handler, client));
-	vTaskDelay(DELAY);
+	ESP_ERROR_CHECK(esp_mqtt_client_register_event(mqtt_client, MQTT_EVENT_PUBLISHED, mqtt_event_handler, mqtt_client));
 
-	std::cout << "Starting MQTT";
-	try_exec(esp_mqtt_client_start(client));
+}
 
-	vTaskDelay(DELAY);*/
-// ################################ END MQTT INITIALIZATION ################################
+void mqtt_start(void){
+    ESP_LOGI(TAG, "Starting MQTT client");
+	ESP_ERROR_CHECK(esp_mqtt_client_start(mqtt_client));
 
+
+}
+
+extern "C" void app_main(void){
+    nvs_init();
+
+    wifi_init();
+
+    wifi_start();
+
+    mqtt_init();
+
+    mqtt_start();
 
 }
